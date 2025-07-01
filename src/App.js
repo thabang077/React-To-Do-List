df9a4a864221c07e955b6b055230aefb93a2a01b
andilebaselinedigital <andile@baselinedigital.co.za>
2025-07-01 13:48:47 +0200

    runLive commit1

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import './App.css';
-import { TodoWrapper } from './components/TodoWrapper';
+import "./App.css";
+import { TodoWrapper } from "./components/TodoWrapper";
 
 function App() {
   return (
     <div className="App">
-      <TodoWrapper  />
+      <TodoWrapper />
     </div>
   );
 }
